99f7b7e56583ce14aa751c0a947e20938ef9a20c
BRayter <gbrayter@gmail.com>
2021-06-15 12:50:43 +0300

    Add Header

---
+++ web-store/src/Components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Sidebar from "./Sidebar/Sidebar";
+
+
+import "./Header.scss";
+
+function Header({ onClick }: any) {
+  return (
+    <div className="Header">
+      <header>
+        <div className="logo">
+          <img
+            src="https://www.tomatina.ua/wp-content/uploads/2020/10/logo_one-1.svg"
+            alt="Tomatina"
+          />
+        </div>
+        <div className="center">
+          <ul className="center-ul">
+            <li>МЕНЮ</li>
+            <li>ЗБЕРИ САМ</li>
+            <li>ДОСТАВКА</li>
+          </ul>
+        </div>
+        <div className="right">
+          <div className="right-left">ЗАЛИШИТИ ВіДГУК</div>
+          <div>
+            <button id="likeButton">❤</button>
+          </div>
+          <div className="right-center">
+            <button id="phoneButton">phone </button>
+            <div>
+              <h4>+38 (063) 117 06 58</h4>
+              <p>Працюємо Пн - Нд з 08:30 до 22:00</p>
+            </div>
+          </div>
+          <div className="right-right">ВХІД</div>
+          <div>
+            <button id="menuButton" onClick={onClick}>
+              menu
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ web-store/src/Components/Header/Sidebar/Sidebar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Sidebar.scss";
+
+function Sidebar({ onClick, active }: any) {
+  return (
+    <div className={active ? "sidebar active" : "sidebar"}>
+      <span>
+        <button onClick={onClick}>×</button>
+      </span>
+      <div>ПРО ТОМАТІНУ</div>
+      <div>ЗБЕРИ САМ</div>
+      <div>ДОСТАВКА</div>
+      <div>ВАКАНСІЇ</div>
+      <div>КОНТАКТИ</div>
+    </div>
+  );
+}
+
+export default Sidebar;
