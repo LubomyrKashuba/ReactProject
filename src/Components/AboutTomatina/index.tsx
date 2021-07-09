5321988507b7c8e5d0e679a32aa52c00240173b9
Artur Kashuba <artur.kashuba13@gmail.com>
2021-07-09 14:47:04 +0300

    add duttonhHeader

--- src/Components/AboutTomatina/index.tsx
+++ src/Components/AboutTomatina/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../Header/Header";
 import "../AboutTomatina/AboutTomatina.scss";
 
 function AboutUs() {
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -9,15 +9,15 @@ function Footer() {
       
       <div className="Footer_component">
         <div className="left-ul">
-        <a href="#">ВАКАНСІЇ</a>
-        <a href="#">Договір Оферта</a>
+        <a href="">ВАКАНСІЇ</a>
+        <a href="">Договір Оферта</a>
         
       </div>
           
       </div>
       <div className="Footer_component">
         <div className="adress">
-        <div id="adress_button"><img src="https://img.icons8.com/android/24/000000/place-marker.png"/></div>
+        <div id="adress_button"><img alt="1" src="https://img.icons8.com/android/24/000000/place-marker.png"/></div>
         <div>
         <h2>м.Львів</h2>
         <p>ТРЦ Victoria Gardens, Кульпарківська 226 А</p>
@@ -29,20 +29,20 @@ function Footer() {
       </div>
       <div className="Footer_component">
         <div className="phone" >
-      <div id="phone_button"><img src="https://img.icons8.com/metro/26/000000/phone.png"/></div>
+      <div id="phone_button"><img alt="1" src="https://img.icons8.com/metro/26/000000/phone.png"/></div>
       <a href="tel:+380631938575">+38 (063) 193 85 75</a>
      
       </div>
       <div className="clock">
-      <div id="clock_button"><img src="https://img.icons8.com/material-sharp/24/000000/clock.png"/></div>
+      <div id="clock_button"><img alt="1" src="https://img.icons8.com/material-sharp/24/000000/clock.png"/></div>
       <h2>Пн - Нд з 08:30 до 22:00</h2>
       </div>
       </div>
       <div className="Footer_component_folove">
         <p>Слідкуй за нами</p>
         <div className="logo">
-        <div id="facebook_button" ><img  src="https://img.icons8.com/fluent/48/000000/facebook-new.png"/></div>
-        <div id="instagram_button"><img src="https://img.icons8.com/plasticine/48/000000/instagram-new--v2.png"/></div>
+        <div id="facebook_button" ><img alt="1" src="https://img.icons8.com/fluent/48/000000/facebook-new.png"/></div>
+        <div id="instagram_button"><img alt="1" src="https://img.icons8.com/plasticine/48/000000/instagram-new--v2.png"/></div>
         </div>
       </div>
     
