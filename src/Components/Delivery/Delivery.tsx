82d8c787d26e197177a8d05dd340f5634b41047e
Artur Kashuba <artur.kashuba13@gmail.com>
2021-07-28 18:56:36 +0300

    fix all

--- src/Components/Delivery/Delivery.tsx
+++ src/Components/Delivery/Delivery.tsx
@@ -8,36 +8,39 @@ function Delivery() {
       <p>
         Доставка та <span>оплата</span>
       </p>
-      <div>
-        <Zone
-          srcZone="https://www.tomatina.ua/wp-content/uploads/2020/10/bike_green.svg"
-          colorZone="Зелена "
-          timeZone="39 "
-          priceZone="250 "
-          priceDelivery="60 "
-        ></Zone>
+      <div className="deliveryTop">
+        <div>
+          <Zone
+            srcZone="https://www.tomatina.ua/wp-content/uploads/2020/10/bike_green.svg"
+            colorZone="Зелена "
+            timeZone="39 "
+            priceZone="250 "
+            priceDelivery="60 "
+          ></Zone>
 
-        <Zone
-          srcZone="https://www.tomatina.ua/wp-content/uploads/2020/11/bike-1.svg"
-          colorZone="Жовта "
-          timeZone="55 "
-          priceZone="250 "
-          priceDelivery="60 "
-        ></Zone>
+          <Zone
+            srcZone="https://www.tomatina.ua/wp-content/uploads/2020/11/bike-1.svg"
+            colorZone="Жовта "
+            timeZone="55 "
+            priceZone="250 "
+            priceDelivery="60 "
+          ></Zone>
 
-        <Zone
-          srcZone="https://www.tomatina.ua/wp-content/uploads/2020/10/bike_red.svg"
-          colorZone="Червона "
-          timeZone="65 "
-          priceZone="350 "
-          priceDelivery="90 "
-        ></Zone>
-      </div>
-      <div className="map">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1286.804504512048!2d24.032541420339346!3d49.8310134470052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1suk!2sua!4v1627224808984!5m2!1suk!2sua"
-          loading="lazy"
-        ></iframe>
+          <Zone
+            srcZone="https://www.tomatina.ua/wp-content/uploads/2020/10/bike_red.svg"
+            colorZone="Червона "
+            timeZone="65 "
+            priceZone="350 "
+            priceDelivery="90 "
+          ></Zone>
+        </div>
+        <div className="map">
+          <iframe
+            title="Map"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1286.804504512048!2d24.032541420339346!3d49.8310134470052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1suk!2sua!4v1627224808984!5m2!1suk!2sua"
+            loading="lazy"
+          ></iframe>
+        </div>
       </div>
       <div className="pay">Оплата</div>
       <div className="payOption">
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,55 +1,67 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import ButtonHeader from "../Header/ButtonHeader/Index";
+import PhoneIphoneIcon from "@material-ui/icons/PhoneIphone";
+import PlaceIcon from "@material-ui/icons/Place";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
 
 import "./Footer.scss";
 
 function Footer() {
   return (
     <div className="Footer">
-      
       <div className="Footer_component">
         <div className="left-ul">
-        
-        <h1>ВАКАНСІЇ</h1> 
-        
-        <h1>Договір Оферта</h1>
-        
-      </div>
-          
+          <Link to="/vacancy">ВАКАНСІЇ</Link>
+
+          <Link to="/Oferta">Договір Оферта</Link>
+        </div>
       </div>
       <div className="Footer_component">
         <div className="adress">
-        <div id="adress_button">
-          
-          <img alt="1" src="https://www.tomatina.ua/wp-content/themes/tomatina/img/icons/contact_icon_one.svg"/></div>
-        <div>
-        <h2>м.Львів</h2>
-        <p>ТРЦ Victoria Gardens, Кульпарківська 226 А</p>
-        <p>ТРЦ Spartak, Мазепи 1Б</p>
-        {/* <h2>с. Сокільники</h2>
-        <p>ТРЦ King Cross, Стрийська 30</p> */}
-        </div>
+          <div id="adress_button">
+            <ButtonHeader icons={<PlaceIcon></PlaceIcon>}></ButtonHeader>
+          </div>
+          <div>
+            <h2>м.Львів</h2>
+            <p>ТРЦ Victoria Gardens, Кульпарківська 226 А</p>
+            <p>ТРЦ Spartak, Мазепи 1Б</p>
+          </div>
         </div>
       </div>
       <div className="Footer_component">
-        <div className="phone" >
-      <div id="phone_button"><img alt="1" src="https://www.tomatina.ua/wp-content/themes/tomatina/img/icons/contact_icon_tree.svg"/></div>
-      <a href="tel:+380631938575">+38 (063) 193 85 75</a>
-     
-      </div>
-      <div className="clock">
-      <div id="clock_button"><img alt="1" src="https://www.tomatina.ua/wp-content/themes/tomatina/img/icons/contact_icon_foure.svg"/></div>
-      <h2>Пн - Нд з 08:30 до 22:00</h2>
-      </div>
+        <div className="phone">
+          <div id="phone_button">
+            <ButtonHeader
+              icons={<PhoneIphoneIcon></PhoneIphoneIcon>}
+            ></ButtonHeader>
+          </div>
+          <a href="tel:+380631938575">+38 (063) 193 85 75</a>
+        </div>
+        <div className="clock">
+          <div id="clock_button">
+            <ButtonHeader
+              icons={<AccessTimeIcon></AccessTimeIcon>}
+            ></ButtonHeader>
+          </div>
+          <h2>Пн - Нд з 08:30 до 22:00</h2>
+        </div>
       </div>
       <div className="Footer_component_folove">
         <h1>Слідкуй за нами</h1>
         <div className="logo">
-        <div id="facebook_button" ><img alt="1" src="https://img.icons8.com/fluent/48/000000/facebook-new.png"/></div>
-        <div id="instagram_button"><img alt="1" src="https://img.icons8.com/plasticine/48/000000/instagram-new--v2.png"/></div>
+          <div id="facebook_button">
+            <ButtonHeader icons={<FacebookIcon></FacebookIcon>}></ButtonHeader>
+          </div>
+          <div id="instagram_button">
+            <ButtonHeader
+              icons={<InstagramIcon></InstagramIcon>}
+            ></ButtonHeader>
+          </div>
         </div>
       </div>
-    
     </div>
   );
 }
